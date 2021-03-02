154e5c2e1f8eee928123070625fa879439870ebd
Samaria Smothers <ssmothers457@noscihigh.org>
2021-03-02 18:05:10 +0000

    2 done

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 /**
  * IN CLASS EXERCISE: LOOPS
  */
-
+var myArray=[1,2,3,4,5]
+var myObject={
+  one:1,
+  two:2,
+  three:3,
+}
 /** 
  * Given an input Array, loop forward over the Array and print its values 
  * using console.log().
  */
 function printArrayValues(array) {
   // YOUR CODE BELOW HERE //
-  
-  
+  for(let i = 0;i < array.length -1; i++){
+
+  }
+  console.log(array[i])
   // YOUR CODE ABOVE HERE //
 }
 
@@ -26,8 +33,10 @@ function printArrayValues(array) {
  */
 function printArrayValuesInReverse(array) {
   // YOUR CODE BELOW HERE //
+for(let i = array.length -1; i >= 0; i--){
 
-  
+}
+  console.log(array[i])
   // YOUR CODE ABOVE HERE //
 }
 
